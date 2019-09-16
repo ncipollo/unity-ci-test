991662afefe1248af13939523bba1273fb6843e6
Nick Cipollo <njc1115@gmail.com>
2019-09-15 21:11:57 -0400

    Add build command

---
+++ Assets/Scripts/Editor/BuildCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public static class BuildCommand {
+    static void PerformBuild() {
+        var options = new BuildPlayerOptions();
+        options.target = BuildTarget.StandaloneOSX;
+
+        var report = BuildPipeline.BuildPlayer(options);
+        var summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded) {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            Debug.Log("Path: " + summary.outputPath);
+        }
+
+        if (summary.result == BuildResult.Failed) {
+            Debug.Log("Build failed");
+        }
+    }
+}
