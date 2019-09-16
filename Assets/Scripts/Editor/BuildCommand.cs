7d134d288334a03e91dc5c333af1893fadd3c7d1
Nick Cipollo <njc1115@gmail.com>
2019-09-15 21:20:18 -0400

    Add build path

--- Assets/Scripts/Editor/BuildCommand.cs
+++ Assets/Scripts/Editor/BuildCommand.cs
@@ -6,8 +6,10 @@ using UnityEditor.Build.Reporting;
 
 public static class BuildCommand {
     static void PerformBuild() {
-        var options = new BuildPlayerOptions();
-        options.target = BuildTarget.StandaloneOSX;
+        var options = new BuildPlayerOptions {
+            locationPathName = "Build/os_x/",
+            target = BuildTarget.StandaloneOSX
+        };
 
         var report = BuildPipeline.BuildPlayer(options);
         var summary = report.summary;
